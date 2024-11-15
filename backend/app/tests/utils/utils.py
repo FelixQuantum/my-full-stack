3eeb2e992f4f04ad072f16baed8f0c1b0ac158b4
duanyadong <duanyadong@corp.netease.com>
2024-11-15 13:36:00 +0800

    ZHANGHAO

--- backend/app/tests/utils/utils.py
+++ backend/app/tests/utils/utils.py
@@ -19,10 +19,13 @@ def get_superuser_token_headers(client: TestClient) -> dict[str, str]:
         "username": settings.FIRST_SUPERUSER,
         "password": settings.FIRST_SUPERUSER_PASSWORD,
     }
-    print(f"{settings.API_V1_STR}/login/access-token",'duanyadong?')
+    
     r = client.post(f"{settings.API_V1_STR}/login/access-token", data=login_data)
     tokens = r.json()
+    print(f"{settings.API_V1_STR}/login/access-token",'duanyadong1')
     print(tokens,'duanyadong2')
+    print(settings.FIRST_SUPERUSER,'duanyadong3')
+    print(settings.FIRST_SUPERUSER_PASSWORD,'duanyadong4')
     a_token = tokens["access_token"]
     headers = {"Authorization": f"Bearer {a_token}"}
     return headers
